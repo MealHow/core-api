aa4aa22ad1856c7cb959c8cdfb6469f7d14e32b8
vvinnych-regrow <vitalii@mealhow.ai>
2023-12-28 14:37:53 +0200

    refactoring

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -4,12 +4,13 @@ from auth0.asyncify import asyncify
 from core.config import get_settings, Settings
 from core.http_client import http_client
 
+settings: Settings = get_settings()
+
 
 def get_auth0_token_client(management_client: bool = False) -> authentication.GetToken:
     """
     Return instance of GetToken.
     """
-    settings: Settings = get_settings()
     get_token_class_wrapper = asyncify(authentication.GetToken)
     get_token = get_token_class_wrapper(
         domain=settings.AUTH0_DOMAIN,
@@ -29,25 +30,25 @@ def get_auth0_users_client() -> authentication.Users:
     """
     Return instance of GetToken.
     """
-    settings: Settings = get_settings()
     users_class_wrapper = asyncify(authentication.Users)
     users = users_class_wrapper(settings.AUTH0_DOMAIN)
     users.set_session(http_client())
     return users
 
 
-async def get_auth0_management_client() -> management.Auth0:
-    """
-    Return instance of Auth0 management API.
-    """
-    settings: Settings = get_settings()
+async def get_auth0_management_api_token() -> str:
     auth0_token = get_auth0_token_client(management_client=True)
     response = await auth0_token.client_credentials_async(
         audience=settings.AUTH0_MANAGEMENT_API_AUDIENCE,
     )
-    mgmt_api_token = response["access_token"]
+    return response["access_token"]
 
+
+async def get_auth0_management_client() -> management.Auth0:
+    """
+    Return instance of Auth0 management API.
+    """
     return management.Auth0(
         domain=settings.AUTH0_DOMAIN,
-        token=mgmt_api_token,
+        token=await get_auth0_management_api_token(),
     )
