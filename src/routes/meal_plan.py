391419458c653d32389941416b2757e272ab08b4
vvinnych-regrow <vitalii@mealhow.ai>
2023-12-30 22:42:26 +0200

    refactoring

--- src/routes/meal_plan.py
+++ src/routes/meal_plan.py
@@ -1,12 +1,9 @@
-import asyncio
-import json
-
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import JSONResponse
 
 from core.config import get_settings, Settings
 from core.dependencies import create_ndb_context
-from services.meal_plan import get_in_progress_meal_plan
+from services.meal_plan import get_in_progress_meal_plan, request_new_meal_plan
 
 router = APIRouter()
 settings: Settings = get_settings()
@@ -20,20 +17,5 @@ async def create_meal_plan(
     if meal_plan:
         return JSONResponse({"message": "A new meal plan is already in progress."}, status_code=409)
 
-    topic = "projects/{project_id}/topics/{topic}".format(
-        project_id=settings.PROJECT_ID,
-        topic=settings.PUBSUB_MEAL_PLAN_EVENT_TOPIC_ID,
-    )
-
-    # TODO: Calculate new calories goal and user's age
-
-    input_data = {"user_id": request.state.user_id}
-    data = json.dumps(input_data).encode("utf-8")
-    request.state.pubsub_publisher.publish(topic, data)
-
-    meal_plan = None
-    while not meal_plan:
-        await asyncio.sleep(1)
-        meal_plan = await get_in_progress_meal_plan(request.state.user_id)
-
-    return JSONResponse({"meal_plan_id": meal_plan.key.id()}, status_code=201)
+    new_meal_plan_id = await request_new_meal_plan(request)
+    return JSONResponse({"meal_plan_id": new_meal_plan_id}, status_code=201)
--- src/services/meal_plan.py
+++ src/services/meal_plan.py
@@ -1,6 +1,14 @@
+import asyncio
+import json
+
+from fastapi import Request
 from google.cloud import ndb
 from mealhow_sdk import enums
 from mealhow_sdk.datastore_models import MealPlan, User
+
+from core.config import get_settings
+
+settings = get_settings()
 
 
 async def get_in_progress_meal_plan(user_id: str) -> MealPlan:
@@ -14,3 +22,23 @@ async def get_in_progress_meal_plan(user_id: str) -> MealPlan:
         )
         .get()
     )
+
+
+async def request_new_meal_plan(request: Request) -> str:
+    topic = "projects/{project_id}/topics/{topic}".format(
+        project_id=settings.PROJECT_ID,
+        topic=settings.PUBSUB_MEAL_PLAN_EVENT_TOPIC_ID,
+    )
+
+    # TODO: Calculate new calories goal and user's age
+
+    input_data = {"user_id": request.state.user_id}
+    data = json.dumps(input_data).encode("utf-8")
+    request.state.pubsub_publisher.publish(topic, data)
+
+    meal_plan = None
+    while not meal_plan:
+        await asyncio.sleep(1)
+        meal_plan = await get_in_progress_meal_plan(request.state.user_id)
+
+    return meal_plan.key.id()
